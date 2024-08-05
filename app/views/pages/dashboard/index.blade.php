025035cab38fba0b421c356acdfb54f5a3c2761b
zahovic Saravia Suarez <zv.elrevelde@gmail.com>
2024-08-05 01:59:41 -0500

    💧 update index dashbard

--- app/views/pages/dashboard/index.blade.php
+++ app/views/pages/dashboard/index.blade.php
@@ -1,2 +1,104 @@
 @extends("layouts.page")
 @section('title','Dashboard')
+@section('content')
+    <div class="row">
+        <div class="col-12 col-lg-9">
+            <div class="row">
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="stats-icon purple mb-2">
+                                        <i class="ph ph-money"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Ventas totales</h6>
+                                    <h6 class="font-extrabold mb-0">0.00</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="stats-icon blue mb-2">
+                                        <i class="ph ph-package"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Productos</h6>
+                                    <h6 class="font-extrabold mb-0">0</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="stats-icon green mb-2">
+                                        <i class="ph ph-tag"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Categorías</h6>
+                                    <h6 class="font-extrabold mb-0">0</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-4 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="stats-icon red mb-2">
+                                        <i class="ph ph-replit-logo"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <h6 class="text-muted font-semibold">Marcas</h6>
+                                    <h6 class="font-extrabold mb-0">0</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-3">
+            <div class="card">
+                <div class="card-body py-4 px-4">
+                    <div class="d-flex align-items-center">
+                        <div class="avatar avatar-xl">
+                            <img src="{{ assets_path("img/icons/colored/w11-profile.png")  }}" alt="profile">
+                        </div>
+                        <div class="ms-3 name">
+                            <h5 class="font-bold">{{ auth()->user()["fullname"] }}</h5>
+                            <h6 class="text-muted mb-0">&#64;{{ auth()->user()["username"] }}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="mb-0">Clientes potenciales</h4>
+                </div>
+                <div class="card-content pb-4" id="container-potential-clients">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@push('js')
+    {{ vite_resource("js/dashboard/client-dashboard.js") }}
+@endpush
