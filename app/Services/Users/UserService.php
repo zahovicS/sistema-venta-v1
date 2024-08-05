b11a1eb3ce2ac01fe7f8e6118a1a6b751ec025fb
zahovic Saravia Suarez <zv.elrevelde@gmail.com>
2024-08-05 01:59:51 -0500

    💧 update user service

--- app/Services/Users/UserService.php
+++ app/Services/Users/UserService.php
@@ -8,7 +8,7 @@ class UserService
 {
 
     /**
-     * Establece la tienda para el usuario, si el usuario es null toma el de la sesión
+     * Establece la tienda al usuario, si el usuario es null toma el de la sesión
      *
      * @param int $store_id
      * @param null|User $user
@@ -26,6 +26,28 @@ class UserService
             $user->save();
             if (is_null($user)) {
                 auth()->update(["store_id" => $store_id]);
+            }
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
+        }
+    }
+    /**
+     * Elimina la tienda asignada el usuario, si el usuario es null toma el de la sesión
+     *
+     * @param null|User $user
+     * @return void
+     * @throws Exception
+     */
+    public function clear_store_to_user(?User $user = null)
+    {
+        try {
+            if (is_null($user)) {
+                $user = User::findOrFail(auth()->user()["id"]);
+            }
+            $user->store_id = null;
+            $user->save();
+            if (is_null($user)) {
+                auth()->update(["store_id" => null]);
             }
         } catch (Exception $exception) {
             throw new Exception($exception->getMessage());
