ad9bb399c9d622a92848b4cd36d5a27f8bb2e9ed
zahovic Saravia Suarez <zv.elrevelde@gmail.com>
2024-08-17 04:44:33 -0500

    💧 actualizacion de rutas

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -20,6 +20,46 @@ app()->group('/stores', [
     }
 ]);
 
+app()->group('/clients', [
+    'namespace' => 'App\Controllers\Clients',
+    'middleware' => 'web-auth',
+    function () {
+        app()->get('/get-top-potential-clients', ['name' => 'clients.get-top-potential-clients', 'middleware' => 'permissionMiddleware', 'ClientController@get_top_potential_clients']);
+    }
+]);
+
+app()->group('/invoices', [
+    'namespace' => 'App\Controllers\Invoices',
+    'middleware' => 'web-auth',
+    function () {
+        app()->post('/get-total-sales', ['name' => 'invoices.get-total-sales', 'InvoiceController@get_total_sales']);
+    }
+]);
+
+app()->group('/categories', [
+    'namespace' => 'App\Controllers\Categories',
+    'middleware' => 'web-auth',
+    function () {
+        app()->get('/get-total-categories', ['name' => 'categories.get-total-categories', 'CategoryController@get_total_categories']);
+    }
+]);
+
+app()->group('/brands', [
+    'namespace' => 'App\Controllers\Brands',
+    'middleware' => 'web-auth',
+    function () {
+        app()->get('/get-total-brands', ['name' => 'brands.get-total-brands', 'BrandController@get_total_brands']);
+    }
+]);
+
+app()->group('/products', [
+    'namespace' => 'App\Controllers\Products',
+    'middleware' => 'web-auth',
+    function () {
+        app()->get('/get-total-products', ['name' => 'products.get-total-products', 'ProductController@get_total_products']);
+    }
+]);
+
 app()->group('/users', [
     'namespace' => 'App\Controllers\Users',
     'middleware' => 'web-auth',
@@ -28,3 +68,5 @@ app()->group('/users', [
         app()->get('/clear-store-user', ['name' => 'users.clear-store-user', 'middleware' => 'permissionMiddleware', 'UserController@clear_store_user']);
     }
 ]);
+
+
