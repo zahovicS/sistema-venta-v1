670c01403ad6bd02d2a1005d06fd29ce909381e1
zahovic Saravia Suarez <zv.elrevelde@gmail.com>
2024-08-17 04:45:52 -0500

    💧 nuevo componente de widget para el dashboard

---
+++ app/views/components/dashboard/widgets.blade.php
@@ -0,0 +1,74 @@
+<div class="row">
+    <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
+            <div class="card-body px-4 py-4-5">
+                <div class="row">
+                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                        <div class="stats-icon purple mb-2">
+                            <i class="ph ph-money"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                        <h6 class="text-muted font-semibold">Ventas totales</h6>
+                        <h6 class="font-extrabold mb-0" id="placeholder-total-sales">0.00</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
+            <div class="card-body px-4 py-4-5">
+                <div class="row">
+                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                        <div class="stats-icon blue mb-2">
+                            <i class="ph ph-package"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                        <h6 class="text-muted font-semibold">Productos</h6>
+                        <h6 class="font-extrabold mb-0" id="placeholder-total-products">0</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
+            <div class="card-body px-4 py-4-5">
+                <div class="row">
+                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                        <div class="stats-icon green mb-2">
+                            <i class="ph ph-tag"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                        <h6 class="text-muted font-semibold">Categorías</h6>
+                        <h6 class="font-extrabold mb-0" id="placeholder-total-categories">0</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-lg-3 col-md-6">
+        <div class="card">
+            <div class="card-body px-4 py-4-5">
+                <div class="row">
+                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                        <div class="stats-icon red mb-2">
+                            <i class="ph ph-replit-logo"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                        <h6 class="text-muted font-semibold">Marcas</h6>
+                        <h6 class="font-extrabold mb-0" id="placeholder-total-brands">0</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('js')
+    {{ vite_resource("js/dashboard/widgets.js") }}
+@endpush
